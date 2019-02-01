2cfe2d569b5202495f54c3ad9c5a2841ac9b309b
Claudio Garaycochea <computadoraweb@gmail.com>
2019-02-01 00:52:19 -0300

    Main structure

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        Play | Stop | Next | Back
+      </div>
+    );
+  }
+}
+
+export default Footer;
