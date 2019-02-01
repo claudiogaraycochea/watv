7d009ab70c1cf59652188ae079fa7d1a48f8dbac
Claudio Garaycochea <computadoraweb@gmail.com>
2019-02-01 11:39:03 -0300

    Redux and Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/main/Main';
+import { Provider } from 'react-redux';
 
 class App extends Component {
   render() {
