239c8ab682222778955bab429c7c2d6b1fe60382
Claudio Garaycochea <computadoraweb@gmail.com>
2019-02-06 23:20:55 -0300

    Testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Main from './components/main/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Main />
+        Testing
       </div>
     );
   }
