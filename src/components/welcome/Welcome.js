9e3fe8bc8e0957410234efad55df90badd078ae9
Claudio Garaycochea <computadoraweb@gmail.com>
2019-06-05 08:56:55 -0300

    New Style

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -6,10 +6,11 @@ import logo from '../../assets/logo-watv.svg';
 class Welcome extends Component {
   render() {
     return (
-      <div className="primary-style">
+      <div className="tertiary-style">
         <div className="container padding-20 center">
           <div className="welcome-wrapper">
             <img src={logo} className="logo" alt="" />
+            <div className="title">WebAnd.TV</div>
             <div className="slogan">Internet and Television Together</div>
           </div>
         </div>
